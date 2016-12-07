e8153270d5d4423716191ae4182d4c4f94966ec3
Zachary Klein <zak@silver-chalice.com>
2016-12-07 13:04:24 -0600

    cleanup, added sample sheet

--- grails-app/controllers/com/ociweb/ExcelUploaderController.groovy
+++ grails-app/controllers/com/ociweb/ExcelUploaderController.groovy
@@ -8,11 +8,9 @@ class ExcelUploaderController {
     def index() { }
 
     def upload() {
-        println "upload...."
         MultipartFile workbook = request.getFile('myFile')
 
         if(workbook) {
-
             def webrootDir = servletContext.getRealPath("/") //app directory
             File fileDest = new File(webrootDir,"uploads/myFile.xls")
             workbook.transferTo(fileDest)
@@ -20,10 +18,7 @@ class ExcelUploaderController {
             BookExcelImporter importer = new BookExcelImporter(fileDest.absolutePath)
 
             def bookRows = importer.getBooks()
-
-
             bookRows.each { props ->
-                println props.toString()
                 def book = new Book(props)
                 if(!book.save(flush: true)) {
                     book.errors.allErrors.each { println it }
